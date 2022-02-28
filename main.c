d019d71896c6e2ef41b4582e15d5a2fac367c1d8
Sharlottaa <dasha.sidorenko1111@mail.ru>
2022-02-28 20:12:43 +0300

    кометрарий

--- main.c
+++ main.c
@@ -15,73 +15,7 @@ char *findS(char *begin) {
     }
     return begin;
 }
-int strlen(const char *s) {
-    char *end = s;
-    while (*end != '\0') {
-        end++;
-    }
-    return end - s;
-}
 
-char *find(char *begin, const char *end, int ch) {
-    while (begin != end && *begin != ch) {
-        begin++;
-    }
-    return begin;
-}
-
-char *findNonSpace(char *begin) {
-    assert(*begin != '\0');
-    while (*begin != '\0' && isspace(*begin)) {
-        begin++;
-    }
-    return begin;
-}
-
-char *findSpace(char *begin) {
-    while (*begin != '\0' && !isspace(*begin)) {
-        begin++;
-    }
-    return begin;
-}
-
-char *findNonSpaceReverse(char *rbegin, const char *rend) {
-    assert(*rbegin != '\0');
-    while (*rbegin >= *rend && isspace(*rend)) {
-        rbegin--;
-    }
-    return rbegin;
-}
-
-char* findSpaceReverse(char *rbegin, const char *rend){
-    while (*rbegin >= *rend && isspace(*rend)) {
-        rbegin--;
-    }
-    return rbegin;
-}
-
-int strcmp(const char *lhs, const char *rhs) {
-    int f;
-    while (*lhs != '\0' && *rhs != '\0') {
-        f = *lhs - *rhs < 0 ? -1 : *lhs - *rhs > 0;
-        lhs++;rhs++;
-    }
-    return f;
-}
-
-char* copy(const char *beginSource, const char *endSource, char *beginDestination) {
-    memcpy(beginDestination, beginSource, endSource - beginSource);
-    return beginDestination + (endSource - beginSource);
-}
-
-
-char* copyIf(char *beginSource, const char *endSource, char *beginDestination, int (*f)(int)){
-    while (beginSource!=endSource){
-        if(f(*beginSource)){
-
-        }
-    }
-}
 
 int main() {
     char *s="Hi mo";
