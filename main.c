abc76fa1de4cf0dac5b1859e4a44a274c193af12
Sharlottaa <dasha.sidorenko1111@mail.ru>
2022-03-03 15:58:04 +0300

    replacingNumbersWithSpaces - tests

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include "string\tasks\removeExtraSpaces.h"
 #include "string\tasks\digitToStart.h"
 #include "string\tasks\LetterToStart.h"
+#include "string\tasks\replacingNumbersWithSpaces.h"
+
 
 void test(){
     test_removeNonLetters();
@@ -14,7 +16,7 @@ void test(){
 
 
 int main() {
-
+    test_replacingNumbersWithSpaces();
 
     return 0;
 }
---
+++ string/tasks/replacingNumbersWithSpaces.h
@@ -0,0 +1,61 @@
+//
+// Created by ну я on 03.03.2022.
+//
+
+#ifndef PROJECT_REPLACINGNUMBERSWITHSPACES_H
+#define PROJECT_REPLACINGNUMBERSWITHSPACES_H
+
+#include "../string_.h"
+
+void replacingNumbersWithSpaces(char *s) {
+    char *end = getEndOfString(s);
+    char *endBuffer = copy(s, end, _stringBuffer);
+    char *readPtr = _stringBuffer;
+
+    while (*readPtr != *endBuffer) {
+        if (isdigit(*readPtr)) {
+            char digit = *readPtr - '0';
+            while (digit != 0) {
+                *(s++) = ' ';
+                digit--;
+            }
+        } else {
+            *(s++) = *readPtr;
+        }
+        readPtr++;
+    }
+    *s = '\0';
+}
+
+void test_replacingNumbersWithSpaces1() {
+    char s[] = "Hi3bro";
+    replacingNumbersWithSpaces(s);
+    ASSERT_STRING("Hi   bro", s);
+}
+
+void test_replacingNumbersWithSpaces2() {
+    char s[] = "Hi1bro";
+    replacingNumbersWithSpaces(s);
+    ASSERT_STRING("Hi bro", s);
+}
+
+void test_replacingNumbersWithSpaces3() {
+    char s[] = "Hi0br1o";
+    replacingNumbersWithSpaces(s);
+    ASSERT_STRING("Hibr o", s);
+}
+
+void test_replacingNumbersWithSpaces4() {
+    char s[] = "1Hib1ro";
+    replacingNumbersWithSpaces(s);
+    ASSERT_STRING(" Hib ro", s);
+}
+
+void test_replacingNumbersWithSpaces(){
+    test_replacingNumbersWithSpaces1();
+    test_replacingNumbersWithSpaces2();
+    test_replacingNumbersWithSpaces3();
+    test_replacingNumbersWithSpaces4();
+}
+
+#endif //PROJECT_REPLACINGNUMBERSWITHSPACES_H
